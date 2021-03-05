85591c741397227775ca7ecba6730671b3a886ec
talimal <talimal@post.bgu.ac.il>
2021-03-05 21:51:49 +0200

    2 upload options

--- src/Components/UploadData.js
+++ src/Components/UploadData.js
@@ -5,18 +5,36 @@ import search from '../images/search.png'
 const UploadData = (props) => {
 
   
-    // const handleFileChosen = (e)=>{
-    //   const file = document.getElementById('fileUpload').files[0];
-    //   console.log(file);
-    // }
+    const handleFileChosen = (e)=>{
+      const file = document.getElementById('actualFile').files[0];
+      console.log(file);
+    }
+
+    const handleNormalFileChosen = (e)=>{
+      const file = document.getElementById('input').files[0];
+      console.log(file);
+    }
   return (
 
-    <div className="fileinputs">
-	    <input type="file" className="file" />
-      <div className="fakefile">
-        <input value="choose file" readOnly={true}/>
-        <img src={search} alt="search"/>
+    <div className="mainDiv">
+      <div className="fileinputs">
+        <input type="file" 
+                className="file"
+                id="actualFile"
+                 />
+        <div className="fakefile">
+          <input value="choose file"
+                  onChange={()=>console.log("heree")}/>
+          <img src={search} alt="search"/>
+        </div>
       </div>
+      <div className="normalInput">
+        <input type="file" id="input"></input>
+      </div>
+      <button className="uploadButtonNormal"
+              onClick={(e)=>handleNormalFileChosen(e)}>Upload1</button>
+       <button className="uploadButtonFake"
+              onClick={(e)=>handleFileChosen(e)}>Upload2</button>
     </div>
   
 
