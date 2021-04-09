ca9754ff7524d2aaa1035ccc1eb33aef21cca76f
talimal <talimal@post.bgu.ac.il>
2021-04-09 22:15:25 +0300

    made the symbols in TIRP matrix bold

--- src/NewProject/TirpMatrix.js
+++ src/NewProject/TirpMatrix.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
+import './TirpMatrix.css';
+
 
 const TirpMatrix = (props) => {
     const relationsJson = {
@@ -10,41 +12,48 @@ const TirpMatrix = (props) => {
     const tirp = props.tirp;
     const symbols = tirp.getSymbols();
     const relations = tirp.getRelations();
-    const matrixRowsIndexes = new Array(symbols.length).fill(0);
+    const matrixRowsIndexes = new Array(symbols.length-1).fill(0);
     const matrixColumnsIndexes = new Array(symbols.length).fill(0);
   
     return (
         <div className="tirpMatrix" >
                 <ReactBootstrap.Table
                     className="table table-bordered" 
-                    style={{'fontSize':'large','background':'#e6ffff'}}
+                    style={{'fontSize':'large','background':'#4ddbff'}}
                     >
-                        <tbody>
-                            {matrixRowsIndexes.map((_ , i)=>{
-                                 return [
-                                    <tr key={i}>
-                                        {i===0?
-                                            symbols.map((_,index)=>{
+                        <thead 
+                            style={{'fontWeight': 'bold'}}>
+                           <tr>
+                                {symbols.map((_,index)=>{
                                                 return[
                                                     <td key={index}>
                                                         {index===0? null:symbols[index]}
                                                     </td>
                                                 ];
-                                            })                                            
-                                        :
+                                })}      
+                           </tr>
+                        </thead>
+                        <tbody>
+                            {matrixRowsIndexes.map((_ , i)=>{
+                                 return [
+                                    <tr key={i}>
+                                        {
                                         matrixColumnsIndexes.map((_ , j)=>{
                                          return [
-                                             <td key={j}>
-                                                 {j===0?symbols[i-1]:
+                                             <td 
+                                                key={j}
+                                                style={j===0?{'fontWeight': 'bold'}:null}
+                                             >
+                                                 {j===0?symbols[i]:
                                                 //  half matrix is empty
-                                                i>1 && i===j+1?
+                                                i>0 && i===j?
                                                  null:
                                                 // symbols.length-1 = # symbols every row
                                                 // (i-1) = because i=0 is the symbols row
                                                 // (j-1) = because j=0 is the symbols column
                                                 // -(i-1) = all the symbols in same row that was
                                                 // deleted due to the condition above (half matrix)
-                                                relationsJson[relations[(symbols.length-1)*(i-1)+(j-1)-(i-1)]]
+                                                relationsJson[relations[(symbols.length-1)*(i)+(j-1)-(i)]]
                                                 }
                                              </td>
                                         ];
